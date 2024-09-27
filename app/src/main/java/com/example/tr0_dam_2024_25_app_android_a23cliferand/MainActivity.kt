b579d8d11c65d301e18971af13bb3671aa06158d
a23cliferand <a23cliferand@inspedralbes.cat>
2024-09-27 18:10:49 +0200

    UPDATE #1

---
+++ app/src/main/java/com/example/tr0_dam_2024_25_app_android_a23cliferand/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tr0_dam_2024_25_app_android_a23cliferand
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tr0_dam_2024_25_app_android_a23cliferand.ui.theme.Tr0dam202425appandroida23cliferandTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tr0dam202425appandroida23cliferandTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Tr0dam202425appandroida23cliferandTheme {
+        Greeting("Android")
+    }
+}
